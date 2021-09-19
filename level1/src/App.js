aef3b7ffcd1f46a61fc10c35bc1d2529e4477a7a
meironY2008 <67857621+meironY2008@users.noreply.github.com>
2021-09-19 20:06:33 +0300

    Update App.js

--- level1/src/App.js
+++ level1/src/App.js
@@ -1,8 +1,11 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 function App() {
+  // ref for the canvas
   const drawingRef = useRef(null);
   const contextRef = useRef(null);
+  //time of click
+  let time;
   useEffect(() => {
     const canvas = drawingRef.current;
     canvas.width = 400;
@@ -10,7 +13,7 @@ function App() {
 
     canvas.style.width = `400px`;
     canvas.style.height = `400px`;
-
+    // libe info
     const context = canvas.getContext("2d");
     context.scale(2, 2);
     context.lineCap = "round";
@@ -18,21 +21,35 @@ function App() {
     context.lineWidth = 3;
     contextRef.current = context;
   }, []);
+  
   const startDrawing = () => {
+    time = Date.now();
+  };
+  const releaseHandeler = () => {
+    // take release time tap
+    let secondTime = Date.now();
     //draw lines
     contextRef.current.beginPath();
-    contextRef.current.moveTo(125, 125);
-    contextRef.current.lineTo(125, 45);
-    contextRef.current.lineTo(45, 125);
+    contextRef.current.moveTo(125, 125 - ((secondTime - time) % 50));
+    contextRef.current.lineTo(
+      125 - ((secondTime - time) % 500),
+      45 + ((secondTime - time) % 500)
+    );
+    contextRef.current.lineTo(
+      45 + ((secondTime - time) % 500),
+      125 - ((secondTime - time) % 500)
+    );
     contextRef.current.closePath();
     contextRef.current.stroke();
   };
-
   return (
+    //element to click on
     <div
-      onClick={startDrawing}
+      onMouseDown={startDrawing}
+      onMouseUp={releaseHandeler}
       style={{ height: "500px", width: "500px", background: "green" }}
     >
+      {/* canvas element */}
       <canvas ref={drawingRef} />
     </div>
   );
