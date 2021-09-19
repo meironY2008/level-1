852f87da5b653910fd84c9faad5ab96f4e6614ac
meironY2008 <67857621+meironY2008@users.noreply.github.com>
2021-09-19 21:21:00 +0300

    level - 5

--- level1/src/App.js
+++ level1/src/App.js
@@ -1,6 +1,7 @@
-import React, { useEffect, useRef,} from "react";
+import React, { useEffect, useRef } from "react";
 
 function App() {
+  //array for the triangles
   const treangeleArray = [];
   // ref for the canvas
   const drawingRef = useRef(null);
@@ -24,6 +25,32 @@ function App() {
     //width of line
     context.lineWidth = 3;
     contextRef.current = context;
+    //get item from local storage
+    const saved = localStorage.getItem("triangle");
+    const initialValue = JSON.parse(saved);
+    if (initialValue != null) {
+      for (let i = 0; i < initialValue.length; i++) {
+        treangeleArray.push(initialValue[i]);
+        contextRef.current.beginPath();
+        //draw first line
+        contextRef.current.moveTo(
+          initialValue[i].pointA.x,
+          initialValue[i].pointA.y
+        );
+        //draw second line
+        contextRef.current.lineTo(
+          initialValue[i].pointB.x,
+          initialValue[i].pointB.y
+        );
+        //draw third line
+        contextRef.current.lineTo(
+          initialValue[i].pointC.x,
+          initialValue[i].pointC.y
+        );
+        contextRef.current.closePath();
+        contextRef.current.stroke();
+      }
+    }
   }, []);
 
   const startDrawing = () => {
@@ -71,7 +98,7 @@ function App() {
   const handeleDeleteButton = () => {
     // pop last triangle from array
     const lastTriangle = treangeleArray.pop();
-    console.log(lastTriangle);
+    //if array of triangles empty he will do nothing
     if (lastTriangle == null) return;
     //change color of line
     contextRef.current.strokeStyle = "green";
@@ -82,12 +109,20 @@ function App() {
     contextRef.current.lineTo(lastTriangle.pointB.x, lastTriangle.pointB.y);
     //delete third line
     contextRef.current.lineTo(lastTriangle.pointC.x, lastTriangle.pointC.y);
+    //end of draw
     contextRef.current.closePath();
+    // draw the lines
     contextRef.current.stroke();
   };
+
+  const handleSaveButton = () => {
+    //save item to local storage
+    localStorage.setItem("triangle", JSON.stringify(treangeleArray));
+  };
   return (
-    //element to click on
+    
     <div>
+      {/* element to click on */}
       <div
         onMouseDown={startDrawing}
         onMouseUp={releaseHandeler}
@@ -98,6 +133,8 @@ function App() {
       </div>
       {/* delete button */}
       <button onClick={handeleDeleteButton}>click for delete</button>
+      {/* save button */}
+      <button onClick={handleSaveButton}>save</button>
     </div>
   );
 }
